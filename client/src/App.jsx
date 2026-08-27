e5b51e745003b3689b33a4718bca3414549d97d3
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-08-27 21:51:10 +0530

    client

--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="h-screen justify-center bg-gray-900 flex items-center">
-      <h1 className="text-4xl font-bold text-blue-400">React + Tailwind v4</h1>
-    </div>
-  );
-}
-
-export default App;
