c91d0fc83656013510b8e5a25844b4db6ccc1678
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-08-27 20:27:02 +0530

    Full stack project

---
+++ devpilot/src/main/java/devPilot/backend/DevpilotApplication.java
@@ -0,0 +1,13 @@
+package devPilot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevpilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevpilotApplication.class, args);
+	}
+
+}
